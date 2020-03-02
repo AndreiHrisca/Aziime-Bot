659b87c5c61e57d33fcd04a34d647e27d8cce0df
AndreiHrisca <46726998+AndreiHrisca@users.noreply.github.com>
2020-03-02 12:24:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -90,6 +90,10 @@ cron.schedule("1 1 1 * * 4", function() {
 
 const momentoact = new Date();
 
+const responses = ['Sa ai o zi minunata!!','Multa bafta ;).', 'Uitate in oglinda inainte sa iesi afara!!', 'Nu uita sa pui haine pe tine.', 'Uneori ai succes…iar alteori înveți.'];
+const respond = () => responses[Math.floor(Math.random() * responses.length)]
+
+
 
 
 
@@ -123,7 +127,7 @@ client.on('message', msg => {
         }
 
         if (msg.content === 'neata') {
-            msg.channel.send(`Buna dimineata si tie ${msg.author} !!! Azi e ` + getToday() + '.');
+            msg.channel.send(`Buna dimineata si tie ${msg.author} !!! Azi e ` + getToday() + respond) ;
         }
 
         // QUESTS / LINKS-------------------------------------------------------------------

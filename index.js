f0ed897dcec3791b00c68ef4cbbfe9c9b8ff2588
AndreiHrisca <46726998+AndreiHrisca@users.noreply.github.com>
2020-04-08 03:41:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ client.on('message', msg => {
         }
 
         if (msg.content === '/critquest') {
-            msg.channel.send(' bad link ');
+            msg.channel.send('https://nwloforums.com/board/4/guides');
         }
 
         if (msg.content === '/stele') {
@@ -230,46 +230,7 @@ client.on('message', msg => {
         }
 });
 
-function image(message, parts) {
-      var search = parts.slice(1).join(" ");
 
-      var options = {
-      url: "http://results.dogpile.com/serp?qc=images&q=" + search,
-      method: "GET",
-      headers: {
-          "Accept": "text/html",
-          "User-Agent": "Chrome"
-      }
-    };
-  request(options, function(error, response, responseBody) {
-      if (error) {
-          // handle error
-          return;
-      }
-
-      /* Extract image URLs from responseBody using cheerio */
-
-      $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
-
-      // In this search engine they use ".image a.link" as their css selector for image links
-      var links = $(".image a.link");
-
-      // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
-      // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
-      var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-      console.log(urls);
-      if (!urls.length) {
-          // Handle no results
-          return;
-      }
-
-      // Send result
-      message.channel.send( urls[0] );
-  });
-
-
-
-}
 //    if (msg.author.id == '256827042278932480' && msg.contant.includes ==='kick') {
 //        member.ban().then((Patrick)),
 //        member.ban.id(256827042278932480)
